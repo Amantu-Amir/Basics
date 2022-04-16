8607369d8b75092118d7fd37b61ae33edece208a
Amantu <42311923+Amantu-Amir@users.noreply.github.com>
2022-04-16 23:39:11 +0600

    Create remove-duplicate.cpp

---
+++ remove-duplicate.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define Mid(b,e) b+(e-b)/2
+#define inf 2147483647
+double PI=2*acos(0.0);
+
+//---------------------------------------------------------------------------//
+//Timus: 314795JS
+
+//knight moves
+//ll X[8] = {2, 1, -1, -2, -2, -1, 1, 2}; 	
+//ll Y[8] = {1, 2, 2, 1, -1, -2, -2, -1}; 
+//ll dx[]={1,-1,0,0};
+//ll dy[]={0,0,1,-1};
+
+void yesNo(bool ck){cout<<(ck? "YES\n":"NO\n");}
+ll toInt(string s){stringstream ss;ss<<s;int n;ss>>n;return n;}
+string toString(ll n){stringstream ss;ss<<n;string s;ss>>s;return s;}
+string UPPER(string s){transform(all(s),s.begin(),::toupper);return s;}
+string LOWER(string s){transform(all(s),s.begin(),::tolower);return s;}
+bool isDigit(char ch){if(ch>=48 && ch<=57) return true; else return false;}
+bool isVowel(char ch){ch=toupper(ch);if(ch=='A'||ch=='U'||ch=='I'||ch=='O'||ch=='E'){return true;}return false;}
+bool isCons(char ch){if(isalpha(ch) && !isVowel(ch)){return true;} return false;}
+
+//------------------------------------------------------------------------------//
+
+const ll p=29;
+const ll N=32768;
+const ll mod=1e9+7;
+
+ll remove_duplicate(vector<ll>&ar, ll len){
+	if(len<=1){
+		return len;
+	}
+	ll last_idx=1;
+	for(ll i=1; i<len; i++){
+		if(ar[i]!=ar[i-1]){
+			ar[last_idx++]=ar[i];
+		}
+	}
+	return last_idx;
+}
+
+
+int main(){
+
+	//freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+	//ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+	ll t=1;
+	for(ll T=1; T<=t; T++){
+		vector<ll>ar={1,1,1,2,2,3,3,4,5,5};
+		ll n=remove_duplicate(ar,ar.size());
+		for(ll i=0; i<n; i++){
+			cout<<ar[i]<<" ";
+		}cout<<"\n";
+	}
+	return 0;
+}
+
