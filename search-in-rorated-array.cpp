a6a4b0c13ab7a50a3bcb0cafccd404e0fa490feb
Amir Hosen <42311923+Amantu-Amir@users.noreply.github.com>
2022-06-21 16:39:17 +0600

    Update search-in-rorated-array.cpp

--- search-in-rorated-array.cpp
+++ search-in-rorated-array.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-    int search(vector<int> &v, int target) {
-        int L=0,R=v.size()-1;
+    int getBreakPoint(vector<int>& nums){
+        int L=0, R=nums.size()-1, mid=0;
+        while(L<=R){
+            mid=(L+R)/2;
+            if(nums[0]<=nums[mid]){
+                L=mid+1;
+            }
+            else{
+                R=mid-1;
+            }
+        }
+        return L;
+    }
+    
+    int search(int L, int R, vector<int>& v, int target){
         while(L<=R){
             int mid=L+(R-L)/2;
             if(v[mid]==target){
                 return mid;
             }
-            if(v[mid]>v[L] or mid==L){
-                if(target<v[mid] and target>=v[L]){
-                    R=mid-1;
-                }
-                else{
-                    L=mid+1;
-                }
+            if(v[mid]>target){
+                R=mid-1;
             }
             else{
-                if(target>v[mid] and target<=v[R]){
-                    L=mid+1;
-                }
-                else{
-                    R=mid-1;
-                }
+                L=mid+1;
             }
         }
         return -1;
     }
+    
+    int search(vector<int>& nums, int target) {
+        int idx=getBreakPoint(nums);
+        int left=search(0, idx-1, nums, target);
+        int right=search(idx, nums.size()-1, nums, target);
+        if(left==-1 and right==-1){
+            return -1;
+        }
+        if(left!=-1){
+            return left;
+        }
+        return right;
+    }
 };
+
+
 
